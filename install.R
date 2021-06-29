9326af9ab41f1d5696d1750031f82f2f705c737d
Sean Brady <sean@massDynamics.com>
2021-06-30 09:29:26 +1000

    adds statmod

--- install.R
+++ install.R
@@ -21,7 +21,7 @@ ensure_package_installed("ggplot2", repos = "http://cran.rstudio.com/")
 ensure_package_installed("forcats", repos = "http://cran.rstudio.com/")
 ensure_package_installed("knitr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("rmarkdown", repos = "http://cran.rstudio.com/")
-
+ensure_package_installed("statmod", repos = "http://cran.rstudio.com/")
 
 #### Install packages Bioconductor
 ensure_package_installed("BiocManager", repos = "http://cran.rstudio.com/")
