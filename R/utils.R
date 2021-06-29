7a660cae2d079629f7ba6423c6ad4b622393d43e
Joseph Isaac Bloom <joseph@massdynamics.com>
2021-06-29 18:27:53 +1000

    prevent colnames changes being made by merge

--- R/utils.R
+++ R/utils.R
@@ -83,11 +83,12 @@ computeReplicateCounts <- function(intensityDF){
 #' the number of replicates and number of imputed values in each condition of interest
 #' @param IntensityExperiment output from `constructSummarizedExperiment`
 #' @param intensityDF Long format table with raw and imputed intensities. Each row is a feature (protein/peptide).
+#' @param conditionComparisonMapping a dataframe matching different limma statistics comparison strings to up and down conditions
 #' @export createCompleteIntensityExperiment
 #' 
 #' @importFrom SummarizedExperiment rowData
 
-createCompleteIntensityExperiment <- function(IntensityExperiment, longIntensityDT){
+createCompleteIntensityExperiment <- function(IntensityExperiment, longIntensityDT, conditionComparisonMapping){
   
   CompleteIntensityExperiment <- IntensityExperiment
   
@@ -101,15 +102,17 @@ createCompleteIntensityExperiment <- function(IntensityExperiment, longIntensity
   rownames(intensityMatrixWide) <- wideIntensityDT$ProteinId
   
   assay(CompleteIntensityExperiment) <- intensityMatrixWide
-
+  
   # add imputed and replicate counts to the final object
   imputedCounts <- computeImputedCounts(longIntensityDT)
-  rowData(CompleteIntensityExperiment) <- merge(rowData(CompleteIntensityExperiment),
-                                                           imputedCounts, by = "ProteinId", all.x = T)
+  rowData(CompleteIntensityExperiment) <- S4Vectors::merge(rowData(CompleteIntensityExperiment),
+                                                as.data.frame(imputedCounts), by = "ProteinId", all.x = T)
   
   replicateCounts <- computeReplicateCounts(longIntensityDT)
-  rowData(CompleteIntensityExperiment) <- merge(rowData(CompleteIntensityExperiment),
-                                                           replicateCounts, by = "ProteinId", all.x = T)
+  rowData(CompleteIntensityExperiment) <- S4Vectors::merge(rowData(CompleteIntensityExperiment),
+                                                as.data.frame(replicateCounts), by = "ProteinId", all.x = T)
+  
+  metadata(CompleteIntensityExperiment)$conditionComparisonMapping <- conditionComparisonMapping
   
   return(CompleteIntensityExperiment)
 }
