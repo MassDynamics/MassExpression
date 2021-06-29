57037e1198503335e751b313292d81cbab9acca6
Sean Brady <sean@massDynamics.com>
2021-06-29 18:09:06 +1000

    updates the R install

--- install.R
+++ install.R
@@ -1,16 +1,19 @@
 #!/usr/bin/Rscript
 
 ensure_package_installed <- function (package, repos = repos) {
-  install.packages(package, repos = repos)
-  library(package, character.only=TRUE)
+  if(!require(package, character.only=TRUE)) {
+    install.packages(package, repos = repos)
+    library(package, character.only=TRUE)
+  }
 }
 
+ensure_package_installed("gert", repos = "http://cran.rstudio.com/")
+ensure_package_installed("usethis", repos = "http://cran.rstudio.com/")
 ensure_package_installed("devtools", repos = "http://cran.rstudio.com/")
 ensure_package_installed("data.table", repos = "http://cran.rstudio.com/")
 ensure_package_installed("foreach", repos = "http://cran.rstudio.com/")
 ensure_package_installed("stringr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("dplyr", repos = "http://cran.rstudio.com/")
-ensure_package_installed("S4Vectors", repos = "http://cran.rstudio.com/")
 ensure_package_installed("jsonlite", repos = "http://cran.rstudio.com/")
 ensure_package_installed("tidyr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("plotly", repos = "http://cran.rstudio.com/")
@@ -21,6 +24,9 @@ ensure_package_installed("rmarkdown", repos = "http://cran.rstudio.com/")
 
 
 #### Install packages Bioconductor
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(c("Biobase", "limma", "GenomicRanges", "GenomeInfoDb", "SummarizedExperiment"))
+ensure_package_installed("BiocManager", repos = "http://cran.rstudio.com/")
+BiocManager::install(c("Biobase", "limma", "S4Vectors", "GenomicRanges", "GenomeInfoDb", "SummarizedExperiment"))
+
+if (!BiocManager::valid()){
+  stop("BiocManager is not valid")
+}
