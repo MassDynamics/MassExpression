aa73ad7eb742e1f3ff02702da8668777562d4e2b
Sean Brady <sean@massDynamics.com>
2021-07-01 09:25:32 +1000

    adds cli dependency

--- install.R
+++ install.R
@@ -22,11 +22,13 @@ ensure_package_installed("forcats", repos = "http://cran.rstudio.com/")
 ensure_package_installed("knitr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("rmarkdown", repos = "http://cran.rstudio.com/")
 ensure_package_installed("statmod", repos = "http://cran.rstudio.com/")
+ensure_package_installed("cli", repos = "http://cran.rstudio.com/")
 
 #### Install packages Bioconductor
 ensure_package_installed("BiocManager", repos = "http://cran.rstudio.com/")
 BiocManager::install(c("Biobase", "limma", "S4Vectors", "GenomicRanges", "GenomeInfoDb", "SummarizedExperiment"))
 
 if (!BiocManager::valid()){
+  BiocManager::valid()$out_of_date
   stop("BiocManager is not valid")
 }
