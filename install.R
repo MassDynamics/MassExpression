1e707f300e28bb4a60170f4a13ffdde478b34ded
Sean Brady <sean@massDynamics.com>
2021-07-01 11:32:53 +1000

    adds more dependencies

--- install.R
+++ install.R
@@ -40,7 +40,8 @@ ensure_package_installed("forcats", repos = "http://cran.rstudio.com/")
 ensure_package_installed("knitr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("rmarkdown", repos = "http://cran.rstudio.com/")
 ensure_package_installed("statmod", repos = "http://cran.rstudio.com/")
-ensure_package_installed_with_version("cli", '3.0.0', repos = "http://cran.rstudio.com/")
+ensure_package_installed_with_version("FactoMineR", '2.4', repos = "http://cran.rstudio.com/")
+ensure_package_installed_with_version("factoextra", '1.0.7', repos = "http://cran.rstudio.com/")
 
 #### Install packages Bioconductor
 ensure_package_installed("BiocManager", repos = "http://cran.rstudio.com/")
