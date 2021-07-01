9db7c6daef7b62b3835d4c264a62896307e7493e
Sean Brady <sean@massDynamics.com>
2021-07-01 10:24:18 +1000

    makes it possible to install particular versions

--- install.R
+++ install.R
@@ -7,9 +7,27 @@ ensure_package_installed <- function (package, repos = repos) {
   }
 }
 
-ensure_package_installed("gert", repos = "http://cran.rstudio.com/")
-ensure_package_installed("usethis", repos = "http://cran.rstudio.com/")
+ensure_package_installed_with_version <- function (package, version, repos = repos) {
+  if(!require(package, character.only=TRUE)) {
+    install_version(package, version = version, repos = repos)
+    library(package, character.only=TRUE)
+  } else if (packageVersion(package) != version) {
+    install_version(package, version = version, repos = repos)
+    library(package, character.only=TRUE)
+  }
+
+  if(packageVersion(package) != version) {
+    stop("issue with: ", package, " ", version)
+  }
+
+}
+
+
 ensure_package_installed("devtools", repos = "http://cran.rstudio.com/")
+
+
+ensure_package_installed("gert", '1.3.1', repos = "http://cran.rstudio.com/")
+ensure_package_installed("usethis", repos = "http://cran.rstudio.com/")
 ensure_package_installed("data.table", repos = "http://cran.rstudio.com/")
 ensure_package_installed("foreach", repos = "http://cran.rstudio.com/")
 ensure_package_installed("stringr", repos = "http://cran.rstudio.com/")
@@ -22,13 +40,16 @@ ensure_package_installed("forcats", repos = "http://cran.rstudio.com/")
 ensure_package_installed("knitr", repos = "http://cran.rstudio.com/")
 ensure_package_installed("rmarkdown", repos = "http://cran.rstudio.com/")
 ensure_package_installed("statmod", repos = "http://cran.rstudio.com/")
-ensure_package_installed("cli", repos = "http://cran.rstudio.com/")
+ensure_package_installed_with_version("cli", '3.0.0', repos = "http://cran.rstudio.com/")
 
 #### Install packages Bioconductor
 ensure_package_installed("BiocManager", repos = "http://cran.rstudio.com/")
 BiocManager::install(c("Biobase", "limma", "S4Vectors", "GenomicRanges", "GenomeInfoDb", "SummarizedExperiment"))
 
 if (BiocManager::valid() != TRUE){
-  print(BiocManager::valid()$out_of_date)
+  BiocManager::valid()$out_of_date
+  sessionInfo()
   stop("BiocManager is not valid")
 }
+
+sessionInfo()
