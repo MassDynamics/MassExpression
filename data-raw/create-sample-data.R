f9a9517549602c8c9833cea6af1e6e8447ea5878
Anna Quaglieri <annaquaglieri@Annas-MacBook-Pro-2.local>
2021-07-02 16:10:13 +1000

    update sample data

--- data-raw/create-sample-data.R
+++ data-raw/create-sample-data.R
@@ -10,7 +10,7 @@ print(here())
 
 
 ################
-# MaxQuant-SILAC
+# MaxQuant-SILAC - NOT INCLUDED IN THE PACKGE
 ################
 
 pg <- read_delim("../../data-s3/data-formats/maxquant/SILAC/paper-sample-dataset/data/proteinGroups.txt",  "\t",
@@ -47,8 +47,8 @@ protein.intensities = read.csv("../../data-s3/data-formats/fragpipe/LFQ/PXD02640
 cols <- colnames(protein.intensities)[grepl("Total.Intensity", colnames(protein.intensities))]
 runs <- gsub(".Total.Intensity", "", cols)
 groups <- gsub("_[0-9]*$","",runs)
-experiment.design <-as_tibble(cbind(cols,runs,groups))
-colnames(experiment.design) <- c("IntensityColumn", "Replicate", "Condition")
+experiment.design <-as_tibble(cbind(cols,groups))
+colnames(experiment.design) <- c("SampleName", "Condition")
 
 protein.id.column = "Protein.ID"
 gene.id.column = "Gene.Names"
@@ -56,7 +56,7 @@ description.id.column = "Description"
 protein.intensities = protein.intensities[,c(protein.id.column,
                                              gene.id.column,
                                              description.id.column,
-                                             experiment.design$IntensityColumn)]
+                                             experiment.design$SampleName)]
 protein.intensities = protein.intensities %>% 
   rename(
     ProteinId = protein.id.column,
@@ -71,7 +71,7 @@ assay_fragpipe <- protein.intensities
 parameters <- data.frame(X1 = c("Species", "UseNormalisationMethod", "LabellingMethod"),
                                 X2 = c("Human","None","LFQ"))
 
-fragpipe_data <- list(intensities = assay_fragpipe, design = design_fragpipe, parameters = parameters)
+fragpipe_data <- list(design = design_fragpipe, intensities = assay_fragpipe, parameters = parameters)
 
 write_delim(assay_fragpipe, "../universal-input-explore/data/fragpipe-lfq/intensities.tsv", delim = "\t")
 save(fragpipe_data, file = "./data/example_fragpipe.rda")
@@ -101,32 +101,45 @@ cols <- colnames(protein.intensities)[grepl("apQuant.Area.", colnames(protein.in
 runs <- gsub("apQuant.Area.", "", cols)
 groups <- str_extract(runs,"sample[0-9]")
 
-experiment.design <- as_tibble(cbind(cols,runs,groups))
-colnames(experiment.design) <- c("IntensityColumn", "Replicate", "Condition")
+experiment.design <- as_tibble(cbind(cols,groups))
+colnames(experiment.design) <- c("SampleName", "Condition")
 experiment.design
 
+extract_genename <- function(description){
+  desl <- strsplit(description, split=" ")[[1]]
+  gn <- desl[sapply(desl, function(z) str_detect(z, "GN="))]
+  gn <- str_remove(gn, "GN=")
+  if(length(gn)==0){
+    gn <- list("")
+  }
+  return(gn)
+}
+
 protein.id.column = "Accession"
-gene.id.column = NULL
 description.id.column = "Description"
 
+protein.intensities$GeneName <- do.call(c, sapply(protein.intensities$Description, function(x) extract_genename(x)))
+
 protein.intensities = protein.intensities[,c(protein.id.column,
-                                             gene.id.column,
                                              description.id.column,
-                                             experiment.design$IntensityColumn)]
+                                             "GeneName",
+                                             experiment.design$SampleName)]
+
 protein.intensities = protein.intensities %>% 
   rename(
     ProteinId = protein.id.column,
-    GeneId = gene.id.column,
     Description = description.id.column
   )
 
 parameters <- data.frame(X1 = c("Species", "UseNormalisationMethod", "LabellingMethod"),
-                                X2 = c("Human","None","LFQ"))
+                                X2 = c("Yeast","None","LFQ"))
 
 
-write_delim(protein.intensities, "../universal-input-explore/data/pd-iRPG/intensities.tsv", delim = "\t")
-write_delim(parameters, "../universal-input-explore/data/pd-iRPG/parameters.tsv", delim = "\t", col_names=FALSE)
-pd_iPRG_data <- list(intensities=protein.intensities, design = experiment.design, parameters=parameters)
+write_delim(protein.intensities, "../data/sample-data-app/generic-importer-input/protein_intensities.tsv", delim = "\t")
+write_delim(parameters, "../data/sample-data-app/generic-importer-input/parameters.tsv", delim = "\t", col_names=FALSE)
+write_delim(experiment.design, "../data/sample-data-app/generic-importer-input/experimental_design.tsv", delim = "\t", col_names=TRUE)
+
+pd_iPRG_data <- list(design = experiment.design, intensities=protein.intensities, parameters=parameters)
 
 save(pd_iPRG_data, file = "./data/example_pd_iRPG.rda")
 
