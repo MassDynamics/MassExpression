f82716080646456792cbf5d1c9ab9e49cc955c2c
Sean Brady <sean@massDynamics.com>
2021-08-04 15:14:27 +1000

    more debug outputs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-mkdir -p renv/cache
+
 
 echo "---------------------------------------"
-
-pwd
+dir=$(pwd)
+RENV_PATHS_CACHE_HOST=$(pwd)/renv/cache
+mkdir -p RENV_PATHS_CACHE_HOST
+ls -al
+ls renv
 ls renv/cache
-ls $(pwd)/renv/cache
-ls $(pwd)/renv/
-
 echo "---------------------------------------"
 
 docker build -t mass_expression .
 
-# the path to an renv cache on the host machine
-RENV_PATHS_CACHE_HOST=$(pwd)/renv/cache
+## the path to an renv cache on the host machine
+#RENV_PATHS_CACHE_HOST=$(pwd)/renv/cache
 
 # the path to the cache within the container
 RENV_PATHS_CACHE_CONTAINER=/usr/local/src/MassExpression/renv/cache
 
 docker run -v "${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER}"  -e "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" mass_expression make installr
+
+ls renv
+ls renv/cache
