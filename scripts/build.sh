326cc6fbf24221e69efec82e434fb4e2c93f2201
Sean Brady <sean@massDynamics.com>
2021-08-04 14:25:44 +1000

    adds a cahce for the build

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -t mass_expression .
+
+
+# the path to an renv cache on the host machine
+RENV_PATHS_CACHE_HOST=$(pwd)/renv/cache
+
+# the path to the cache within the container
+RENV_PATHS_CACHE_CONTAINER=/usr/local/src/MassExpression/renv/cache
+
+docker run -v "${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER}"  -e "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" mass_expression R -e 'renv::restore()'
